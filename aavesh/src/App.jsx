336e1852e1e214243008b51ec0c7d9a4deaca27b
Vaibhav <madaanvaibhav001@gmail.com>
2024-09-23 00:09:44 +0530

    Created the Aavesh team Page

--- aavesh/src/App.jsx
+++ aavesh/src/App.jsx
@@ -1,13 +1,19 @@
-import Carousel from "./components/Carousel";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
+import Home  from "./pages/Home";
+// import Carousel from "./components/Carousel";
+// import Header from "./components/Header";
+// import Hero from "./components/Hero";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import TeamPage from "./pages/TeamPage";
 
 function App() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Carousel/>
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/team' element={<TeamPage />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ aavesh/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Carousel from "../components/Carousel";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <>
+       <Header />
+      <Hero />
+      <Carousel/>
+    </>
+  )
+}
+
+export default Home;
