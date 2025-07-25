0ef59519f4fc84edc632f997fbe62fbc8ed4114c
pulkitsujaan <pulkitsujaan2708@gmail.com>
2025-07-25 23:02:20 +0530

    fixed sponsors component

--- aavesh/src/components/Sponsors.jsx
+++ aavesh/src/components/Sponsors.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import {sponsors} from '../constants/sponsors'
 
 // Decorator components remain the same
 const DecoratorCircleFirst = ({ className }) => (
@@ -20,13 +20,7 @@ const DecoratorLinesFirst = ({ className }) => (
 DecoratorLinesFirst.propTypes = {
   className: PropTypes.string,
 };
-const sponsors = [
-  { id: 1, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[4] col-start-[2] sm:row-start-[5] sm:col-start-[2]` },
-  { id: 2, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[3] col-start-[7] sm:row-start-[3] sm:col-start-[8]` },
-  { id: 3, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[7] col-start-[4] sm:row-start-[7] sm:col-start-[5]` },
-  { id: 4, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[8] col-start-[8] sm:row-start-[8] sm:col-start-[8]` },
-];
-// --- CHANGE END ---
+//sponsers now imported from constants
 
 const Sponsors = () => {
   return (
--- aavesh/src/constants/sponsors.js
+++ aavesh/src/constants/sponsors.js
@@ -1,22 +1,6 @@
-export const SPONSORS = [
-  {
-    id: 1,
-    name: 'Sponsor 1',
-    style: { top: '20%', left: '20%' },
-  },
-  {
-    id: 2,
-    name: 'Sponsor 2',
-    style: { top: '10%', left: '60%' },
-  },
-  {
-    id: 3,
-    name: 'Sponsor 3',
-    style: { top: '45%', left: '45%' },
-  },
-  {
-    id: 4,
-    name: 'Sponsor 4',
-    style: { top: '60%', left: '70%' },
-  },
+export const sponsors = [
+  { id: 1, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[4] col-start-[2] sm:row-start-[5] sm:col-start-[2]` },
+  { id: 2, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[3] col-start-[7] sm:row-start-[3] sm:col-start-[8]` },
+  { id: 3, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[7] col-start-[4] sm:row-start-[7] sm:col-start-[5]` },
+  { id: 4, name: 'Coca-Cola', imageUrl: '/coke.png', gridSize:`row-start-[8] col-start-[8] sm:row-start-[8] sm:col-start-[8]` },
 ];
