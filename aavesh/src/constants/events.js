a849d9b0a10e64d3e50ac5689ff92feca6ee5ae6
Deep Shekhar Singh <deepshekhar0306@gmail.com>
2025-08-27 22:15:59 +0530

    Upcoming  events updated

--- aavesh/src/constants/events.js
+++ aavesh/src/constants/events.js
@@ -1,20 +1,8 @@
 export const EVENTS_DATA = [
   {
-    image: 'https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'ARD-WIN-O',
-    date: '12 DEC 2024',
-    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
-  },
-  {
-    image: 'https://images.pexels.com/photos/1181354/pexels-photo-1181354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'TECH FORWARD',
-    date: '25 JAN 2025',
-    description: 'Join us for a look into the future of technology. A day full of inspiring keynotes, hands-on workshops, and networking opportunities with leaders from around the globe. Discover the next wave of innovation.',
-  },
-  {
-    image: 'https://images.pexels.com/photos/3184465/pexels-photo-3184465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-    title: 'MARKETING SUMMIT',
-    date: '18 MAR 2025',
-    description: 'The premier event for marketing professionals. Learn the latest strategies in digital marketing, content creation, and brand building from the experts who are shaping the industry.',
+    image: '/comingSoon.jpg',
+    title: 'Coming-Soon',
+    date: '--/--/--',
+    description: 'Yet to be announced',
   },
 ];
