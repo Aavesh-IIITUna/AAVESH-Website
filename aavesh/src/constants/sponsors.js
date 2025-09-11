5a043abd093c9a3a0019269de028a2c65f2870dd
Pranav <gargpranav851@gmail.com>
2025-09-12 02:04:53 +0530

    feat(sponsors): update sponsor list and adjust component sizing
    - Add new sponsor 'Etani Cafe' with corresponding image
    - Rename existing sponsors and adjust their positions in the grid
    - Reduce size of sponsor logos in the Sponsors component
    - Add a new center-positioned sponsor

--- aavesh/src/constants/sponsors.js
+++ aavesh/src/constants/sponsors.js
@@ -1,6 +1,7 @@
 export const sponsors = [
-  { id: 1, name: 'Coca-Cola', imageUrl: '/sponsor.webp', gridSize:`row-start-[4] col-start-[2] sm:row-start-[5] sm:col-start-[2]` },
-  { id: 2, name: 'Coca-Cola', imageUrl: '/sponsor2.webp', gridSize:`row-start-[3] col-start-[7] sm:row-start-[3] sm:col-start-[8]` },
-  { id: 3, name: 'Coca-Cola', imageUrl: '/chaiboozelogo.webp', gridSize:`row-start-[7] col-start-[4] sm:row-start-[7] sm:col-start-[5]` },
-  { id: 4, name: 'Coca-Cola', imageUrl: '/sbi.webp', gridSize:`row-start-[8] col-start-[8] sm:row-start-[8] sm:col-start-[8]` },
+  { id: 1, name: 'Samba', imageUrl: '/sponsor.webp', gridSize:`row-start-[5] col-start-[2] sm:row-start-[5] sm:col-start-[2]` },   // left
+  { id: 2, name: 'Fatafat', imageUrl: '/sponsor2.webp', gridSize:`row-start-[4] col-start-[8] sm:row-start-[4] sm:col-start-[8]` }, // right top
+  { id: 3, name: 'Etani Cafe', imageUrl: '/etanicafe.webp', gridSize:`row-start-[7] col-start-[4] sm:row-start-[7] sm:col-start-[4]` }, // bottom left
+  { id: 4, name: 'SBI', imageUrl: '/sbi.webp', gridSize:`row-start-[8] col-start-[7] sm:row-start-[8] sm:col-start-[7]` }, // bottom right
+  { id: 5, name: 'New Sponsor', imageUrl: '/chaiboozelogo.webp', gridSize:`row-start-[5] col-start-[5] sm:row-start-[5] sm:col-start-[5]` }, // center
 ];
